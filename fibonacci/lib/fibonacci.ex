87ba5398d1495adf2d31a08b837614862e553cce
jlorek <lorek.jens@gmail.com>
2021-01-02 20:09:24 +0100

    fibfib

--- fibonacci/lib/fibonacci.ex
+++ fibonacci/lib/fibonacci.ex
@@ -2,15 +2,20 @@ defmodule Fibonacci do
   # def fib(0), do: 0
   # def fib(1), do: 1
 
-  def fib(agent, n) do
-    # fib(n - 1) + fib(n - 2)
-    {cache_hit, cache_value} = FibCache.try_get(agent, n)
+  def fib(n) do
+    agent = FibCache.start()
+    result = fib(agent, n)
+    Agent.stop(agent)
+    result
+  end
 
-    cond do
-      cache_hit == true ->
+  defp fib(agent, n) do
+    # fib(n - 1) + fib(n - 2)
+    case FibCache.try_get(agent, n) do
+      {_cache_hit = true, cache_value} ->
         cache_value
 
-      cache_hit == false ->
+      {_cache_hit = false, _} ->
         value = fib(agent, n - 1) + fib(agent, n - 2)
         FibCache.update(agent, {n, value})
         value
